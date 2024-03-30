f4f5fd194e9b28eb6db832c024a65932cd196812
Mohammad Sarraf <m.sheikhsarraf@gmail.com>
2024-03-30 22:28:49 +0100

    add the logging massage

--- ranging_sensor_gui/utils/utils.py
+++ ranging_sensor_gui/utils/utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datasets.RangingSensor import RangingData
+import logging
+from Settings.GUI_settings import SAVE_DATA_MESSAGE
+
 
 def save_data_to_csv(data:RangingData) -> None:
     data_df = pd.DataFrame(data)
     data_df.to_csv('data.csv', index=False)
+    logging.info(SAVE_DATA_MESSAGE.SAVE_DATA_MESSEAGE)
