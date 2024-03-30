27b318ba9562b399665e9db06731da149ffc71bd
Mohammad Sarraf <m.sheikhsarraf@gmail.com>
2024-03-30 22:26:33 +0100

    add save data massage

--- ranging_sensor_gui/Settings/GUI_settings.py
+++ ranging_sensor_gui/Settings/GUI_settings.py
@@ -4,5 +4,8 @@ class ERROR_ESTATES(Enum):
     SERIAL_ERROR = -1
 
 class ERROR_MESSAGE:
-    SERIAL_ERROR_MESSSAGE = "[ERROR] Serial port is not connected!"
+    SERIAL_ERROR_MESSSAGE = "Serial port is not connected!"
+
+class SAVE_DATA_MESSAGE:
+    SAVE_DATA_MESSEAGE = "Data saved"
     
